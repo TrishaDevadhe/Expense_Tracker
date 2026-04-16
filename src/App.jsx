4e8d7f5477693f5a4fabdf8beb56be990d09cb6c
TrishaDevadhe <trishadevadhe@gmail.com>
2026-04-16 18:42:49 +0530

    chore: trigger fresh deployment build

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Deployment Trigger: Build v2
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
